443458b67dbdd96af75c889f302f7018de973499
andybak <andy@andybak.net>
2020-07-29 16:19:13 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,69 @@
+import {Deck} from '@deck.gl/core';
+import {GeoJsonLayer, ArcLayer} from '@deck.gl/layers';
+import {EditableGeoJsonLayer, DrawPolygonMode} from '@nebula.gl/layers';
+
+const COUNTRIES =  'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_scale_rank.geojson'; //eslint-disable-line
+const AIR_PORTS = 'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson';
+
+const INITIAL_VIEW_STATE = {
+  latitude: 51.47,
+  longitude: 0.45,
+  zoom: 4,
+  bearing: 0,
+  pitch: 30
+};
+
+var myFeatureCollection = {
+  type: 'FeatureCollection',
+  features: [
+    /* insert features here */
+  ]
+};
+
+const selectedFeatureIndexes = [];
+
+export const deck = new Deck({
+  initialViewState: INITIAL_VIEW_STATE,
+  controller: true,
+  layers: [
+    new GeoJsonLayer({
+      id: 'base-map',
+      data: COUNTRIES,
+      // Styles
+      stroked: true,
+      filled: true,
+      lineWidthMinPixels: 2,
+      opacity: 0.4,
+      getLineColor: [60, 60, 60],
+      getFillColor: [200, 200, 200]
+    }),
+    new GeoJsonLayer({
+      id: 'airports',
+      data: AIR_PORTS,
+      // Styles
+      filled: true,
+      pointRadiusMinPixels: 2,
+      pointRadiusScale: 2000,
+      getRadius: f => 11 - f.properties.scalerank,
+      getFillColor: [200, 0, 80, 180],
+      // Interactive props
+      pickable: true,
+      autoHighlight: true,
+      onClick: info =>
+        // eslint-disable-next-line
+        info.object && alert(`${info.object.properties.name} (${info.object.properties.abbrev})`)
+    }),
+
+    new EditableGeoJsonLayer({
+      id: 'nebula',
+      data: myFeatureCollection,
+      mode: DrawPolygonMode,
+      selectedFeatureIndexes,
+      onEdit: ({ updatedData }) => {console.log(updatedData);}
+    })
+  ]
+});
+
+// For automated test cases
+/* global document */
+document.body.style.margin = '0px';
