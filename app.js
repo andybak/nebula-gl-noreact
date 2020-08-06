3ce9e28c832a5b2c6019dc84595b091d5acc58a7
andybak <andy@andybak.net>
2020-08-06 10:29:31 +0100

    Some interactivity

--- app.js
+++ app.js
@@ -15,81 +15,101 @@ const INITIAL_VIEW_STATE = {
 
 var myFeatureCollection = {
   "type": "FeatureCollection",
-  "features": [ ]
-};
-
-const layers = [
-  new GeoJsonLayer({
-    id: 'base-map',
-    data: COUNTRIES,
-    // Styles
-    stroked: true,
-    filled: true,
-    lineWidthMinPixels: 2,
-    opacity: 0.4,
-    getLineColor: [60, 60, 60],
-    getFillColor: [200, 200, 200]
-  }),
-  new GeoJsonLayer({
-    id: 'airports',
-    data: AIR_PORTS,
-    // Styles
-    filled: true,
-    pointRadiusMinPixels: 2,
-    pointRadiusScale: 2000,
-    getRadius: f => 11 - f.properties.scalerank,
-    getFillColor: [200, 0, 80, 180],
-    // Interactive props
-    pickable: true,
-    autoHighlight: true,
-    onClick: info =>
-        // eslint-disable-next-line
-        info.object && alert(`${info.object.properties.name} (${info.object.properties.abbrev})`)
-  }),
-
-  new EditableGeoJsonLayer({
-    id: 'nebula',
-    data: myFeatureCollection,
-    selectedFeatureIndexes : [],
-    mode: 'drawPolygon',
-    onEdit: ({ updatedData, editType, featureIndexes, editContext }) => {
-
-      if (editType === 'addFeature') {
-
-        myFeatureCollection = updatedData;
-
-        layers.push(new GeoJsonLayer({
-
-          id: 'extruded',
-          data: updatedData.features,
-
-          // Styles
-          filled: true,
-          pointRadiusMinPixels: 2,
-          pointRadiusScale: 2000,
-          extruded: true,
-          getElevation: 1000000,
-          getFillColor: [200, 0, 80, 180],
-
-          // Interactive props
-          pickable: true,
-          autoHighlight: true,
-
-        }));
-
-        deck.setProps({layers : layers})
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [-23.94, 52.58],
+            [-14.31, 48.84],
+            [-16.59, 55.08],
+            [-23.94, 52.58]
+          ]
+        ]
       }
-
-    },
-  }),
-]
+    }
+  ]
+};
 
 export const deck = new Deck({
   initialViewState: INITIAL_VIEW_STATE,
   controller: true,
-  layers: layers
 });
+
+deck.setProps({layers : getLayers(myFeatureCollection, 'view')})
+
+function getLayers(features, mode) {
+
+    console.log(myFeatureCollection);
+
+    var editableLayer = new EditableGeoJsonLayer({
+        id: 'nebula',
+        data: features,
+        selectedFeatureIndexes : [features.features.length - 1],
+        mode: mode,
+        onEdit: ({ updatedData, editType, featureIndexes, editContext }) => {
+            console.log(editType)
+            if (editType!=='addTentativePosition') {
+            myFeatureCollection = updatedData;
+            deck.setProps({layers : getLayers(myFeatureCollection, 'view')})
+            }
+        },
+      });
+
+    var baseLayer = new GeoJsonLayer({
+        id: 'base-map',
+        data: COUNTRIES,
+        // Styles
+        stroked: true,
+        filled: true,
+        lineWidthMinPixels: 2,
+        opacity: 0.4,
+        getLineColor: [60, 60, 60],
+        getFillColor: [200, 200, 200]
+      });
+
+    return [
+        baseLayer,
+        editableLayer,
+    ]
+}
+
+window.addEventListener("keydown", function (event) {
+
+  if (event.defaultPrevented) {
+    return; // Do nothing if the event was already processed
+  }
+
+  switch (event.key.toUpperCase()) {
+    case "C":
+        deck.setProps({layers : getLayers(myFeatureCollection, 'drawCircleFromCenter')})
+        break;
+    case "X":
+        deck.setProps({layers : getLayers(myFeatureCollection, 'extrude')})
+        break;
+    case "E":
+        deck.setProps({layers : getLayers(myFeatureCollection, 'elevation')})
+        break;
+    case "V":
+        deck.setProps({layers : getLayers(myFeatureCollection, 'view')})
+        break;
+    case "D":
+        deck.setProps({layers : getLayers(myFeatureCollection, 'drawPolygon')})
+        break;
+    case "M":
+        deck.setProps({layers : getLayers(myFeatureCollection, 'modify')})
+      break;
+    default:
+      return;
+  }
+
+  event.preventDefault();
+}, true);
 
 // For automated test cases
 /* global document */
 document.body.style.margin = '0px';
+
